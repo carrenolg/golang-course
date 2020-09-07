7d8a10862ea6f29a019f773d7e1de816e19db62a
carrenolg <carrenolg@hotmail.com>
2020-09-07 14:25:34 -0500

    working with Go modules

--- usingModules/hello.go
+++ usingModules/hello.go
@@ -1,8 +1,13 @@
 package hello
 
-import "rsc.io/quote"
+import "rsc.io/quote/v3"
 
 // Hello return "Hello, world"
 func Hello() string {
-	return quote.Hello()
+	return quote.HelloV3()
+}
+
+// Proverb retunr a Go concurrency proverb
+func Proverb() string {
+	return quote.Concurrency()
 }
